da8ecdf00b1ce4356919e0511a1cae7892efa184
Antoine Abonneau <antoineabonneau94@gmail.com>
2023-06-28 16:12:40 +0200

    Added progress bar

--- src/zip_tools.c
+++ src/zip_tools.c
@@ -1,1 +1,15 @@
 #include "zip_tools.h"
+
+void printProgressBar(int current, int total) {
+    const int barWidth = 70;
+
+    printf("\r[");
+    int pos = (int)((float)barWidth * current / total);
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) printf("=");
+        else if (i == pos) printf(">");
+        else printf(" ");
+    }
+    printf("] %d%%", (int)((float)current * 100.0 / total));
+    fflush(stdout);  // Force the line to print
+}
